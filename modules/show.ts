2728e77be11419ddaa8ca0ee89ffcdb197e93a64
BackRunner <dev@backrunner.top>
2021-04-15 23:01:59 +0800

    feat: simplify show command

--- modules/show.ts
+++ modules/show.ts
@@ -6,19 +6,16 @@ import { getDevConfig, getRuntimeConfig } from '../utils/env';
 const showRc = async (app: Application) => {
   const rc = await getRuntimeConfig(app.workDir);
   if (!rc) {
-    app.logger.error('Cannot get content in .tigorc, the file may not exist.');
+    // try to get dev config
+    const devConfig = getDevConfig(app);
+    if (!devConfig.content) {
+      app.logger.error('Cannot find any configuration file in current directory.');
+      return;
+    }
+    console.log(chalk.green(`Here's your .tigodev content: \n`), JSON.stringify(devConfig.content, null, '  '));
     return;
   }
   console.log(chalk.green(`Here's your .tigorc content: \n`), JSON.stringify(rc, null, '  '));
-};
-
-const showDevConfig = async (app: Application) => {
-  const devConfig = getDevConfig(app);
-  if (!devConfig.content) {
-    app.logger.error('Cannot get content in .tigodev, the file may not exist.');
-    return;
-  }
-  console.log(chalk.green(`Here's your .tigodev content: \n`), JSON.stringify(devConfig.content, null, '  '));
 };
 
 const mount = async (app: Application, program: commander.Command): Promise<void> => {
@@ -27,15 +24,9 @@ const mount = async (app: Application, program: commander.Command): Promise<void
     .description('Show the content of some files (like .tigorc)');
   cmd
     .command('rc')
-    .description('Show content in the .tigorc')
+    .description('Show content in the .tigorc or .tigodev')
     .action(() => {
       showRc(app);
-    });
-  cmd
-    .command('devrc')
-    .description('Show content in the .tigodev')
-    .action(() => {
-      showDevConfig(app);
     });
 };
 
