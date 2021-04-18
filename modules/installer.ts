c3c7c313204d9dd22d4dc66ffaeafe9df910c001
BackRunner <dev@backrunner.top>
2021-04-18 17:05:24 +0800

    feat: update postInstall methods

--- modules/installer.ts
+++ modules/installer.ts
@@ -3,74 +3,12 @@ import NpmApi from 'npm-api';
 import child_process from 'child_process';
 import path from 'path';
 import fs from 'fs';
+import inquirer from 'inquirer';
 import { Application } from '../interface/application';
 import { checkServerDir, getRuntimeConfig, getRuntimeConfigStatus, writeRuntimeConfig } from '../utils/env';
-import inquirer from 'inquirer';
-import shelljs from 'shelljs';
-import { Logger } from 'log4js';
-import { RuntimeConfig, RuntimeConfigStatus } from '../interface/rc';
+import { buildPostInstallThisArg } from '../utils/postInstall';
 
 const npm = new NpmApi();
-
-interface postInstallThis {
-  inquirer: typeof inquirer;
-  npm: typeof npm;
-  workDir: string;
-  shell: typeof shelljs;
-  logger: Logger;
-  rc: {
-    status: RuntimeConfigStatus;
-    content: RuntimeConfig;
-    write: (status: RuntimeConfigStatus, rc: RuntimeConfig) => void;
-  };
-  getPluginConfig: (name: string) => unknown;
-  updatePluginConfig: (name: string, operation: (config: unknown) => void) => void;
-  saveRuntimeConfig: () => void;
-}
-
-const buildPostInstallThisArg = (app: Application, rcStatus: RuntimeConfigStatus, rc: RuntimeConfig): postInstallThis => {
-  const getPluginConfig = (name): unknown => {
-    if (!rc.plugins) {
-      return null;
-    }
-    const installedPlugins = Object.keys(rc.plugins);
-    for (const pluginName of installedPlugins) {
-      if (rc.plugins[pluginName].package === name) {
-        return rc.plugins[pluginName].config || {};
-      }
-    }
-    return null;
-  };
-  const saveRuntimeConfig = (): void => {
-    writeRuntimeConfig(rcStatus, rc);
-  };
-  return {
-    inquirer,
-    workDir: app.workDir,
-    npm,
-    shell: shelljs,
-    logger: app.logger,
-    rc: {
-      status: rcStatus,
-      content: rc,
-      write: writeRuntimeConfig,
-    },
-    getPluginConfig,
-    updatePluginConfig: (name, operation): void => {
-      const config = getPluginConfig(name);
-      if (config) {
-        if (typeof operation === 'function') {
-          operation(config);
-        }
-        saveRuntimeConfig();
-        app.logger.info('Runtime config has been updated.');
-      } else {
-        app.logger.error('Cannot get the content of runtime config, please set your manually.');
-      }
-    },
-    saveRuntimeConfig,
-  };
-};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const fetchPackageInfo = async (app: Application, prefix: string, moduleName: string): Promise<any> => {
