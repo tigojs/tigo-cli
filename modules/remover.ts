f5fda962d2f0e1801102b21640e803566aad297d
BackRunner <dev@backrunner.top>
2021-04-18 23:09:35 +0800

    fix: type error

--- modules/remover.ts
+++ modules/remover.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import child_process from 'child_process';
 import { Application } from '../interface/application';
 import { checkServerDir, getRuntimeConfig, getRuntimeConfigStatus, writeRuntimeConfig } from '../utils/env';
+import { RuntimeConfig } from '../interface/rc';
 
 const mount = async (app: Application, program: commander.Command): Promise<void> => {
   program
@@ -24,12 +25,7 @@ const mount = async (app: Application, program: commander.Command): Promise<void
       const dependencies = Object.keys(packageInfo.dependencies || {});
       const devDependencies = Object.keys(packageInfo.devDependencies || {});
       const isPrefixed = dependencies.includes(prefixedName) || devDependencies.includes(prefixedName);
-      if (
-        !dependencies.includes(prefixedName) &&
-        !dependencies.includes(moduleName) &&
-        !devDependencies.includes(prefixedName) &&
-        !devDependencies.includes(moduleName)
-      ) {
+      if (!dependencies.includes(prefixedName) && !dependencies.includes(moduleName) && !devDependencies.includes(prefixedName) && !devDependencies.includes(moduleName)) {
         app.logger.error('This module is not installed on the server.');
         return;
       }
@@ -43,15 +39,17 @@ const mount = async (app: Application, program: commander.Command): Promise<void
       // remove configuration
       app.logger.debug('Processing the runtime config...');
       const rcStatus = getRuntimeConfigStatus(app.workDir);
-      const rc = await getRuntimeConfig(rcStatus);
-      const pluginNames = Object.keys(rc.plugins);
-      for (const name of pluginNames) {
-        if (rc.plugins[name].package === (isPrefixed ? prefixedName : moduleName)) {
-          delete rc.plugins[name];
-          break;
+      const rc = <RuntimeConfig>await getRuntimeConfig(rcStatus);
+      if (rc.plugins) {
+        const pluginNames = Object.keys(rc.plugins);
+        for (const name of pluginNames) {
+          if (rc.plugins[name].package === (isPrefixed ? prefixedName : moduleName)) {
+            delete rc.plugins[name];
+            break;
+          }
         }
+        writeRuntimeConfig(rcStatus, rc);
       }
-      writeRuntimeConfig(rcStatus, rc);
       app.logger.info('Runtime config has been updated.');
       app.logger.info('Module has been removed successfully.');
     });
