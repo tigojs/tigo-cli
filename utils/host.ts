463ec8fcc79fc378641248e6f61fb0b2f8f99898
BackRunner <dev@backrunner.top>
2021-04-24 18:25:12 +0800

    fix: parse port

--- utils/host.ts
+++ utils/host.ts
@@ -1,6 +1,7 @@
 interface Host {
   https: boolean,
   host: string,
+  port?: number,
 }
 
 export const parseHost = (host: string): Host => {
@@ -11,6 +12,11 @@ export const parseHost = (host: string): Host => {
   ret.host = host.replace(/^https?:\/\//, '');
   if (ret.host.endsWith('/')) {
     ret.host = ret.host.substr(0, ret.host.length - 1);
+  }
+  if (ret.host.includes(':')) {
+    const parts = ret.host.split(':');
+    ret.host = parts[0];
+    ret.port = parseInt(parts[1], 0);
   }
   return ret;
 };
