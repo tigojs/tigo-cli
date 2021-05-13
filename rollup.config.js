bc1baa957a4d5c2dab3a0b5cc559eaa9ed69c2ce
BackRunner <dev@backrunner.top>
2021-05-13 22:31:33 +0800

    fix: rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import json from '@rollup/plugin-json';
 import fs from 'fs';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'));
-const external = Object.keys(pkg.dependencies || {}).concat(['fs/promises']);
+const external = Object.keys(pkg.dependencies || {}).concat(['fs/promises', 'mysql2/promise']);
 
 const extensions = ['.js', '.ts'];
 
